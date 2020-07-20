ed5b75c1b78ab4e2aebf96d24cb1d1f218dbdc2c
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-20 18:13:27 -0300

    feat: adding BaseEntity.cs

---
+++ src/ReviewApi/ReviewApi.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace ReviewApi.Domain.Entities
+{
+    internal class BaseEntity
+    {
+        public int Id { get; protected set; }
+        public bool Deleted { get; protected set; }
+
+        public void SetAsDeleted() 
+        {
+            Deleted = true; 
+        }
+
+        public BaseEntity()
+        {
+
+        }
+
+        public BaseEntity(int id)
+        {
+            Id = id; 
+        }
+    }
+}
