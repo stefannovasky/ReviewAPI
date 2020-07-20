5b7163fcfef8512f88d9d46987cc34a583d3ccd0
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-20 18:03:12 -0300

    feat: adding initial project template

---
+++ src/ReviewApi/ReviewApi.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ReviewApi.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/ReviewApi/ReviewApi.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ReviewApi.Shared
+{
+    public class Class1
+    {
+    }
+}
