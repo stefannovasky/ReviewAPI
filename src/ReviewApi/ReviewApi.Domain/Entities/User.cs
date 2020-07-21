236c370eca09593539c338cc70bd30423abec059
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-20 23:06:01 -0300

    feat: adding User entity

---
+++ src/ReviewApi/ReviewApi.Domain/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿namespace ReviewApi.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+
+        public User()
+        {
+
+        }
+
+        public User(int id) : base(id)
+        {
+
+        }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public User(int id, string name, string email, string password) : base(id)
+        {
+            Name = name;
+            Email = email;
+            Password = password; 
+        }
+
+        public void UpdatePassword(string password)
+        {
+            Password = password; 
+        }
+    }
+}
