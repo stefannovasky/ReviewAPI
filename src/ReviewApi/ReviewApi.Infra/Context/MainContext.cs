4c7dd15f7f15309c549abc8bae0559195d60d504
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-21 20:16:01 -0300

    feat: adding application DbContext

---
+++ src/ReviewApi/ReviewApi.Infra/Context/MainContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReviewApi.Infra.Context
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public MainContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MainContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer("DefaultConnection");
+            }
+        }
+    }
+}
