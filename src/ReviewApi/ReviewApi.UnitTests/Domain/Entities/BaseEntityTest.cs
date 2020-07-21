93e736dd40e4d46275c20c7aca32d827c7749a24
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-20 23:27:40 -0300

    test: adding BaseEntity tests

---
+++ src/ReviewApi/ReviewApi.UnitTests/Domain/Entities/BaseEntityTest.cs
@@ -0,0 +1,37 @@
+﻿using ReviewApi.Domain.Entities;
+using Xunit;
+
+namespace ReviewApi.UnitTests.Domain.Entities
+{
+    public class BaseEntityTest
+    {
+        [Fact]
+        public void ShouldConstructBaseEntityWithEmptyConstructor()
+        {
+            BaseEntity baseEntity = new BaseEntity();
+
+            Assert.Equal(0, baseEntity.Id);
+            Assert.False(baseEntity.Deleted);
+        }
+
+        [Fact]
+        public void ShouldConstructBaseEntityWithIdConstructor()
+        {
+            int id = 1;
+            BaseEntity baseEntity = new BaseEntity(id);
+
+            Assert.Equal(id, baseEntity.Id);
+            Assert.False(baseEntity.Deleted);
+        }
+
+        [Fact]
+        public void ShouldDelete()
+        {
+            BaseEntity baseEntity = new BaseEntity();
+
+            baseEntity.SetAsDeleted();
+
+            Assert.True(baseEntity.Deleted);
+        }
+    }
+}
