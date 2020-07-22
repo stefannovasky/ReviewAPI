5f7c05af9696d121139d94308efa03838abc9ecf
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-21 21:46:56 -0300

    feat: adding ApiStatusController tests

---
+++ src/ReviewApi/ReviewApi.IntegrationTests/Controllers/ApiStatusControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using ReviewApi.IntegrationTests.CustomWebApplicationFactory;
+using Xunit;
+
+namespace ReviewApi.IntegrationTests.Controllers
+{
+    [Collection("Sequential")]
+    public class ApiStatusControllerTest : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _webApplicationFactory;
+        private readonly HttpClient _httpClient;
+
+        public ApiStatusControllerTest(CustomWebApplicationFactory<Startup> webApplicationFactory)
+        {
+            _webApplicationFactory = webApplicationFactory;
+            _httpClient = _webApplicationFactory.CreateClient(new WebApplicationFactoryClientOptions());
+        }
+
+        [Fact]
+        public async Task ShouldReturnOkStatusCodeOnCallApiStatus()
+        {
+            HttpResponseMessage httpResponse = await _httpClient.GetAsync("../api/");
+
+            Assert.Equal((int)HttpStatusCode.OK, (int)httpResponse.StatusCode);
+        }
+    }
+}
