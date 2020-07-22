0e5c7c51569dbdc7c98344a3355413e956bd69cc
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-21 21:48:26 -0300

    feat: adding ApiStatusController

---
+++ src/ReviewApi/ReviewApi/Controllers/ApiStatusController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReviewApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiStatusController : ControllerBase
+    {
+        public IActionResult Get() => Ok();
+    }
+}
