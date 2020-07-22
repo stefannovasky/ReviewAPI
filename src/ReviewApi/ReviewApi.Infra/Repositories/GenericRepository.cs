79b2b6dfbfc2ce4927a562cba9249ec66d2afbd0
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-22 03:18:22 -0300

    feat: adding GenericRepository

---
+++ src/ReviewApi/ReviewApi.Infra/Repositories/GenericRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReviewApi.Domain.Entities;
+using ReviewApi.Domain.Interfaces.Repositories;
+using ReviewApi.Infra.Context;
+
+namespace ReviewApi.Infra.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly MainContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(MainContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task Create(T entity)
+        {
+            try
+            {
+                await _dbSet.AddAsync(entity);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            try
+            {
+                return await Query().SingleOrDefaultAsync(entity => !entity.Deleted && entity.Id == id);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public async Task Save()
+        {
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public void Update(T entity)
+        {
+            try
+            {
+                _dbSet.Update(entity);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        protected IQueryable<T> Query() => _dbSet.AsNoTracking();
+    }
+}
