c9a39a936550633981a56156477b01c501d41a42
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-22 03:17:14 -0300

    feat: adding GenericRepository interface

---
+++ src/ReviewApi/ReviewApi.Domain/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using ReviewApi.Domain.Entities;
+
+namespace ReviewApi.Domain.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetById(int id);
+        Task Create(T entity);
+        void Update(T entity);
+        Task Save();
+    }
+}
