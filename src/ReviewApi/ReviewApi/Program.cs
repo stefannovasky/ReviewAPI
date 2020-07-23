c441d42304e6a61c4af8a060d7a061e990c1e144
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-23 15:21:54 -0300

    feat: adding EmailWorker on project

--- src/ReviewApi/ReviewApi/Program.cs
+++ src/ReviewApi/ReviewApi/Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ReviewApi.Infra.Redis.Interfaces;
+using ReviewApi.Shared.Interfaces;
+using ReviewApi.Workers;
+using ReviewApi.Workers.Configurations;
 
 namespace ReviewApi
 {
@@ -15,6 +21,13 @@ namespace ReviewApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureServices(services =>
+                {
+                    IConfigurationRoot configs = new ConfigurationBuilder().AddJsonFile("appsettings.json", false).Build();
+
+                    services.AddHostedService<EmailWorker>(worker => new EmailWorker(
+                       worker.GetRequiredService<IRedisConnector>(), worker.GetRequiredService<IJsonUtils>(), configs.GetSection("EmailConfiguration").Get<EmailConfiguration>()
+                    ));
                 });
     }
 }
