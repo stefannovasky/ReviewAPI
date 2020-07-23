ee00cdc48f1fa4ad700d3a1afada195032fc8a79
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-22 21:44:24 -0300

    feat: adding HashUtils

---
+++ src/ReviewApi/ReviewApi.Shared/Interfaces/IHashUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace ReviewApi.Shared.Interfaces
+{
+    public interface IHashUtils
+    {
+        string GenerateHash(string plaintext);
+        bool CompareHash(string plaintext, string hash);
+    }
+}
---
+++ src/ReviewApi/ReviewApi.Shared/Utils/HashUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using ReviewApi.Shared.Interfaces;
+
+namespace ReviewApi.Shared.Utils
+{
+    public class HashUtils : IHashUtils
+    {
+        public bool CompareHash(string plaintext, string hash)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                plaintext = GenerateHash(plaintext);
+                return plaintext == hash;
+            }
+        }
+
+        public string GenerateHash(string plaintext)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plaintext));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
