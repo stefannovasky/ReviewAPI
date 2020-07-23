a0b388ec9b617ee06df478eeacb72ebf2ab8e554
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-22 21:29:24 -0300

    feat: adding AlreadyExistsException

---
+++ src/ReviewApi/ReviewApi.Domain/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReviewApi.Domain.Exceptions
+{
+    public class AlreadyExistsException : Exception
+    {
+        public AlreadyExistsException(string entityName) : base($"{entityName} already exists.")
+        {
+
+        }
+    }
+}
