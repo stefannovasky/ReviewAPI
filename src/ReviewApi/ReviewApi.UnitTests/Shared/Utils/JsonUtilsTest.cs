c144cb055d967b52e2080e2270be758b8f4eea73
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-23 15:17:19 -0300

    test: adding JsonUtils tests

---
+++ src/ReviewApi/ReviewApi.UnitTests/Shared/Utils/JsonUtilsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ReviewApi.Domain.Entities;
+using ReviewApi.Shared.Interfaces;
+using ReviewApi.Shared.Utils;
+using Xunit;
+
+namespace ReviewApi.UnitTests.Shared.Utils
+{
+    public class JsonUtilsTest
+    {
+        private readonly IJsonUtils _jsonUtils; 
+        public JsonUtilsTest()
+        {
+            _jsonUtils = new JsonUtils();
+        }
+
+        [Fact]
+        public void ShouldSerializeObject()
+        {
+            object obj = new { Key = "Value" };
+
+            string json = _jsonUtils.Serialize(obj);
+
+            Assert.Equal("{\"Key\":\"Value\"}", json);
+        }
+
+        [Fact]
+        public void ShouldDeserializeObject()
+        {
+            Guid guid = Guid.NewGuid();
+            BaseEntity obj = new BaseEntity(guid);
+
+            string json = _jsonUtils.Serialize(obj);
+            BaseEntity deserialized = _jsonUtils.Deserialize<BaseEntity>(json);
+
+            Assert.Equal(guid, deserialized.Id);
+        }
+    }
+}
