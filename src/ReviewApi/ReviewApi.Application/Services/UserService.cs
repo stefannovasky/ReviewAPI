7a6e277fcc4bbfd7d59d544b08bca28430df3250
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-22 22:00:02 -0300

    feat: adding Create method on UserService

---
+++ src/ReviewApi/ReviewApi.Application/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using ReviewApi.Application.Interfaces;
+using ReviewApi.Application.Models.User;
+using ReviewApi.Domain.Entities;
+using ReviewApi.Domain.Exceptions;
+using ReviewApi.Domain.Interfaces.Repositories;
+using ReviewApi.Shared.Interfaces;
+
+namespace ReviewApi.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IConfirmationCodeUtils _confirmationCodeUtils;
+        private readonly IHashUtils _hashUtils;
+
+        public UserService(IUserRepository userRepository, IConfirmationCodeUtils confirmationCodeUtils, IHashUtils hashUtils)
+        {
+            _userRepository = userRepository;
+            _confirmationCodeUtils = confirmationCodeUtils;
+            _hashUtils = hashUtils;
+        }
+
+        public async Task Create(CreateUserRequestModel model)
+        {
+            User user = new User(model.Name, model.Email, model.Password);
+            if (await _userRepository.AlreadyExists(user.Email))
+            {
+                throw new AlreadyExistsException("user");
+            }
+            user.UpdateConfirmationCode(_confirmationCodeUtils.GenerateConfirmationCode());
+            user.UpdatePassword(_hashUtils.GenerateHash(user.Password));
+            await _userRepository.Create(user);
+            await _userRepository.Save();
+        }
+    }
+}
