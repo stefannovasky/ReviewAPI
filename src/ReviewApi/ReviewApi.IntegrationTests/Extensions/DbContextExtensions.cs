d8cd814699c3bb46d2e2500d1cf8ea2ae29dcf69
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-23 16:17:59 -0300

    test: adding ResetDatabase method

---
+++ src/ReviewApi/ReviewApi.IntegrationTests/Extensions/DbContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReviewApi.IntegrationTests.Extensions
+{
+    internal static class DbContextExtensions
+    {
+        public static void ResetDatabase(this DbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
