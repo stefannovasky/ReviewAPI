378a7360ecf31c8001675c2cb71ad46f5b21b240
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-22 21:26:57 -0300

    feat: adding ConfirmationCodeUtils

---
+++ src/ReviewApi/ReviewApi.Shared/Utils/ConfirmationCodeUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using ReviewApi.Shared.Interfaces;
+
+namespace ReviewApi.Shared.Utils
+{
+    public class ConfirmationCodeUtils : IConfirmationCodeUtils
+    {
+        public string GenerateConfirmationCode()
+        {
+            int length = 8;
+
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+
+            char letter;
+
+            for (int i = 0; i < length; i++)
+            {
+                double next = random.NextDouble();
+                int shift = Convert.ToInt32(Math.Floor(25 * next));
+                letter = Convert.ToChar(shift + 65);
+                builder.Append(letter);
+            }
+
+            return builder.ToString().ToUpper();
+        }
+    }
+}
