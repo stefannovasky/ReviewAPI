05e5e06f6b43774b319353c9195a40f79d6ddedf
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-23 15:20:31 -0300

    feat: adding EmailWorker

---
+++ src/ReviewApi/ReviewApi.Workers/Configurations/EmailConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace ReviewApi.Workers.Configurations
+{
+    public class EmailConfiguration
+    {
+        public string From { get; set; }
+        public string SmtpServer { get; set; }
+        public string Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
