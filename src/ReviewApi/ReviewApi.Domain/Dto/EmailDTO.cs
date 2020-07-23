45c0377ebdfdc75af3e30117ccbab30e46b332bb
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-23 15:18:56 -0300

    feat: adding EmailDTO

---
+++ src/ReviewApi/ReviewApi.Domain/Dto/EmailDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace ReviewApi.Domain.Dto
+{
+    public class EmailDTO
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public EmailDTO(string to, string subject, string body)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+    }
+}
