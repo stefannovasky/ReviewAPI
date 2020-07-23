685fd52d4a899aff42f9dfb1aedbf80de4a0dd0b
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-22 21:58:26 -0300

    feat: adding CreateUserRequestModel

---
+++ src/ReviewApi/ReviewApi.Application/Models/User/CreateUserRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewApi.Application.Models.User
+{
+    public class CreateUserRequestModel
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
