38d860b140d13f9011c38781b2d1aea1086225d8
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-23 22:48:37 -0300

    feat: adding CreateUserValidator

---
+++ src/ReviewApi/ReviewApi.Application/Validators/Extensions/AbstractValidatorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ReviewApi.Application.Validators.Extensions
+{
+    public static class AbstractValidatorExtensions
+    {
+        public static async Task ValidateRequestModelAndThrow<T>(this AbstractValidator<T> validator, T requestModel)
+        {
+            ValidationResult validationResponse = await validator.ValidateAsync(requestModel);
+            if (!validationResponse.IsValid)
+            {
+                StringBuilder builder = new StringBuilder();
+                ValidationFailure lastElement = validationResponse.Errors[validationResponse.Errors.Count - 1];
+                validationResponse.Errors.Remove(lastElement);
+                validationResponse.Errors.ToList().ForEach(e => builder.AppendLine(e.ErrorMessage));
+                builder.Append(lastElement.ErrorMessage);
+                string errors = builder.ToString();
+                throw new ValidationException(errors.Replace(errors));
+            }
+        }
+    }
+}
