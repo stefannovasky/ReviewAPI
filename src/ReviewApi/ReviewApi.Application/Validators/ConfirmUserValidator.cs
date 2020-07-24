0c564dcd0adaade25a616c45afbb15ef2e427064
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-24 02:20:25 -0300

    feat: adding ConfirmUserValidator

---
+++ src/ReviewApi/ReviewApi.Application/Validators/ConfirmUserValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ReviewApi.Application.Models.User;
+
+namespace ReviewApi.Application.Validators
+{
+    public class ConfirmUserValidator : AbstractValidator<ConfirmUserRequestModel>
+    {
+        public ConfirmUserValidator()
+        {
+            RuleFor(p => p.Code)
+                .NotNull().WithMessage("code cannot be null.")
+                .NotEmpty().WithMessage("code cannot be empty.")
+                .Length(8).WithMessage("code must be 8 characters.");
+        }
+    }
+}
