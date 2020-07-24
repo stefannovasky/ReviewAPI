5dd2f0334ed346be60f3b46deb07cdb0d4c50de3
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-24 20:04:35 -0300

    feat: adding UserResponseModel, Authentication request and response model

---
+++ src/ReviewApi/ReviewApi.Application/Models/User/AuthenticationUserResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReviewApi.Application.Models.User
+{
+    public class AuthenticationUserResponseModel
+    {
+        public UserResponseModel User { get; set; }
+        public string Token { get; set; }
+    }
+}
