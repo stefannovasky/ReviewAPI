5cb65f881d6dd1f6a0e8032de9ed820a7af89c66
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-24 00:16:28 -0300

    feat: adding UserNotConfimedException

---
+++ src/ReviewApi/ReviewApi.Domain/Exceptions/UserNotConfirmedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReviewApi.Domain.Exceptions
+{
+    public class UserNotConfirmedException : Exception
+    {
+        public UserNotConfirmedException() : base("email already registered, needs to be confirmed.") 
+        {
+
+        }
+    }
+}
