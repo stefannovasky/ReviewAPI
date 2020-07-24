3e175007b3e12ca903130dcd7d5432b4b68ba2ae
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-24 20:08:11 -0300

    test: adding JwtTokenUtils tests

---
+++ src/ReviewApi/ReviewApi.UnitTests/Shared/Utils/JwtTokenUtilsTest.cs
@@ -0,0 +1,23 @@
+﻿using ReviewApi.Shared.Interfaces;
+using ReviewApi.Shared.Utils;
+using Xunit;
+
+namespace ReviewApi.UnitTests.Shared.Utils
+{
+    public class JwtTokenUtilsTest
+    {
+        private readonly IJwtTokenUtils _jwtTokenUtils;
+
+        public JwtTokenUtilsTest()
+        {
+            _jwtTokenUtils = new JwtTokenUtils("secret_key_secret_key_secret_key");
+        }
+
+        [Fact]
+        public void ShouldGenerateToken()
+        {
+            string token = _jwtTokenUtils.GenerateToken("user id");
+            Assert.True(token.Length > 100);
+        }
+    }
+}
