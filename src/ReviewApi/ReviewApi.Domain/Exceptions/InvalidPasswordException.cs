03bc7a689647cffcc30adda2d1dac858323df2fa
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-24 20:08:36 -0300

    feat: adding InvalidPasswordException

---
+++ src/ReviewApi/ReviewApi.Domain/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReviewApi.Domain.Exceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException() : base("incorret password.")
+        {
+
+        }
+    }
+}
