eca7214feb0411dbdabb755f3305d03b2f6bbb22
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-27 22:02:06 -0300

    feat: adding UpdateNameUserValidator

---
+++ src/ReviewApi/ReviewApi.Application/Validators/User/UpdateNameUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using ReviewApi.Application.Models.User;
+
+namespace ReviewApi.Application.Validators.User
+{
+    public class UpdateNameUserValidator : AbstractValidator<UpdateNameUserRequestModel>
+    {
+        public UpdateNameUserValidator()
+        {
+            RuleFor(u => u.Name)
+               .NotNull().WithMessage("name cannot be null.")
+               .NotEmpty().WithMessage("name cannot be empty.")
+               .MinimumLength(3).WithMessage("name must be greater than 2 characters.")
+               .MaximumLength(150).WithMessage("name must be less than 150 characters.");
+        }
+    }
+}
