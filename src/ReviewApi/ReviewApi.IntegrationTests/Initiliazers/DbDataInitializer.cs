a46d4d60bb4834ebf36b7e90bc1b2e24422ceb21
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-28 10:52:57 -0300

    test: adding database initializers

---
+++ src/ReviewApi/ReviewApi.IntegrationTests/Initiliazers/DbDataInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewApi.Domain.Entities;
+using ReviewApi.Infra.Context;
+
+namespace ReviewApi.IntegrationTests.Initiliazers
+{
+    internal class DbDataInitializer<T> where T : BaseEntity
+    {
+        protected MainContext _dbContext;
+        protected DbSet<T> _dbSet;
+
+        protected void Save() => _dbContext.SaveChanges();
+    }
+}
---
+++ src/ReviewApi/ReviewApi.IntegrationTests/Initiliazers/UsersDbDataInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using ReviewApi.Domain.Entities;
+using ReviewApi.Infra.Context;
+
+namespace ReviewApi.IntegrationTests.Initiliazers
+{
+    internal class UsersDbDataInitializer : DbDataInitializer<User>
+    {
+        public List<User> InsertedsConfirmedUsers { get; set; }
+
+        public UsersDbDataInitializer(MainContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<User>();
+
+            CreateConfirmedUsersInDatabase();
+        }
+
+        public void CreateConfirmedUsersInDatabase()
+        {
+            InsertedsConfirmedUsers = new List<User>()
+            {
+                new User("User Name 1", "user1@mail.com", "user password"),
+                new User("User Name 2", "user2@mail.com", "user password"),
+                new User("User Name 3", "user3@mail.com", "user password")
+            };
+
+            foreach (User user in InsertedsConfirmedUsers)
+            {
+                _dbSet.Add(user);
+                _dbContext.SaveChanges();
+                user.Confirm();
+                _dbSet.Update(user);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+}
