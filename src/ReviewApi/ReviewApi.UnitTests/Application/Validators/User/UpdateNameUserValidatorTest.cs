f56005b9b8c9720b2b1f9962a2304ec8fd5302a7
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-27 22:02:22 -0300

    test: adding UpdateNameUserValidator tests
    	[Theory]
            [InlineData("")]
            [InlineData(" ")]
            public void ShouldHaveValidationErrorWithEmptyName(string name)
            {
                CreateUserRequestModel requestModel = new CreateUserRequestModel()
                {
                    Name = name
                };
    
                _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name cannot be empty.");
            }
    
            [Theory]
            [InlineData(null)]
            public void ShouldHaveValidationErrorWithNullName(string name)
            {
                CreateUserRequestModel requestModel = new CreateUserRequestModel()
                {
                    Name = name
                };
    
                _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name cannot be empty.");
            }
    
            [Theory]
            [InlineData("aa")]
            [InlineData("a")]
            public void ShouldHaveValidationErrorWithLessThan3CharactersName(string name)
            {
                CreateUserRequestModel requestModel = new CreateUserRequestModel()
                {
                    Name = name
                };
    
                _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name must be greater than 2 characters.");
            }
    
            [Theory]
            [InlineData("greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. ")]
            public void ShouldHaveValidationErrorWithGreatherThan150CharactersName(string name)
            {
                CreateUserRequestModel requestModel = new CreateUserRequestModel()
                {
                    Name = name
                };
    
                _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name must be less than 150 characters.");
            }reateUserValidator tests

---
+++ src/ReviewApi/ReviewApi.UnitTests/Application/Validators/User/UpdateNameUserValidatorTest.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation.TestHelper;
+using ReviewApi.Application.Models.User;
+using ReviewApi.Application.Validators.User;
+using Xunit;
+
+namespace ReviewApi.UnitTests.Application.Validators.User
+{
+    public class UpdateNameUserValidatorTest
+    {
+        private readonly UpdateNameUserValidator _validator;
+        public UpdateNameUserValidatorTest()
+        {
+            _validator = new UpdateNameUserValidator(); 
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ShouldHaveValidationErrorWithEmptyName(string name)
+        {
+            UpdateNameUserRequestModel requestModel = new UpdateNameUserRequestModel()
+            {
+                Name = name
+            };
+
+            _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name cannot be empty.");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        public void ShouldHaveValidationErrorWithNullName(string name)
+        {
+            UpdateNameUserRequestModel requestModel = new UpdateNameUserRequestModel()
+            {
+                Name = name
+            };
+
+            _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name cannot be empty.");
+        }
+
+
+        [Theory]
+        [InlineData("aa")]
+        [InlineData("a")]
+        public void ShouldHaveValidationErrorWithLessThan3CharactersName(string name)
+        {
+            UpdateNameUserRequestModel requestModel = new UpdateNameUserRequestModel()
+            {
+                Name = name
+            };
+
+            _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name must be greater than 2 characters.");
+        }
+
+        [Theory]
+        [InlineData("greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. greather than 150 characters. ")]
+        public void ShouldHaveValidationErrorWithGreatherThan150CharactersName(string name)
+        {
+            UpdateNameUserRequestModel requestModel = new UpdateNameUserRequestModel()
+            {
+                Name = name
+            };
+
+            _validator.ShouldHaveValidationErrorFor(r => r.Name, requestModel).WithErrorMessage("name must be less than 150 characters.");
+        }
+    }
+}
