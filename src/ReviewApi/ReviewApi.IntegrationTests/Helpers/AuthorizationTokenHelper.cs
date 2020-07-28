8df056255d4f327dff5e3dca41c27c8f22e80f6f
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-28 10:53:29 -0300

    test: adding AuthorizationTokenHelper

---
+++ src/ReviewApi/ReviewApi.IntegrationTests/Helpers/AuthorizationTokenHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ReviewApi.Shared.Utils;
+
+namespace ReviewApi.IntegrationTests.Helpers
+{
+    internal class AuthorizationTokenHelper
+    {
+        public string CreateToken(Guid userId)
+        {
+            string token = new JwtTokenUtils("ajsdhausd62313gshaJJJ").GenerateToken(userId.ToString());
+            return $"Bearer {token}";
+        }
+    }
+}
