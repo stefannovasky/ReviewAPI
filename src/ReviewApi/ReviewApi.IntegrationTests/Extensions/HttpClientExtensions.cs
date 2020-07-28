1c0e20e8586c9fc04a34d9ce4492142bb5aefb2e
stefannovasky <stefan.novasky@hbsis.com.br>
2020-07-28 10:54:15 -0300

    feat: adding InsertAuthorizationTokenOnRequestHeader method

---
+++ src/ReviewApi/ReviewApi.IntegrationTests/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace ReviewApi.IntegrationTests.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static void InsertAuthorizationTokenOnRequestHeader(this HttpClient client, string token)
+        {
+            client.DefaultRequestHeaders.Add("Authorization", token);
+        }
+    }
+}
