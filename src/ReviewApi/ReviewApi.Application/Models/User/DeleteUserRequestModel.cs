f15e1d863e55a546981d737ed0f9844fbe4503d4
Stefan Novasky <stefannovasky@gmail.com>
2020-07-29 23:04:38 -0300

    feat: adding DeleteUserRequestModel

---
+++ src/ReviewApi/ReviewApi.Application/Models/User/DeleteUserRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReviewApi.Application.Models.User
+{
+    public class DeleteUserRequestModel
+    {
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+    }
+}
