d4e84554710d996d1151d156a009a9a55863f770
Stefan Novasky <stefannovasky@gmail.com>
2020-07-29 23:04:59 -0300

    feat: adding DeleteUserValidator

---
+++ src/ReviewApi/ReviewApi.Application/Validators/User/DeleteUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using ReviewApi.Application.Models.User;
+
+namespace ReviewApi.Application.Validators.User
+{
+    public class DeleteUserValidator : AbstractValidator<DeleteUserRequestModel>
+    {
+        public DeleteUserValidator()
+        {
+            RuleFor(u => u.Password)
+                .NotNull().WithMessage("password cannot be null.")
+                .NotEmpty().WithMessage("password cannot be empty.")
+                .MinimumLength(8).WithMessage("password must be greater than 8 characters.")
+                .MaximumLength(60).WithMessage("password must be less than 60 characters.");
+            RuleFor(u => u.PasswordConfirmation)
+                .Equal(u => u.Password).WithMessage("password confirmation must be equal to new password.");
+        }
+    }
+}
