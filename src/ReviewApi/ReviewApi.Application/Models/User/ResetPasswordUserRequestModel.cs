332eae93ff1dc439aa2d7d35aa251540ad5a9fa8
Stefan Novasky <stefannovasky@gmail.com>
2020-07-30 22:34:55 -0300

    feat: adding ResetPasswordUserRequestModel

---
+++ src/ReviewApi/ReviewApi.Application/Models/User/ResetPasswordUserRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewApi.Application.Models.User
+{
+    public class ResetPasswordUserRequestModel
+    {
+        public string Code { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirmation { get; set; }
+    }
+}
