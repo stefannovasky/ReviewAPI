a829f4881a6b82f0063fda96a00f5db2d6d65ce2
Stefan Novasky <stefannovasky@gmail.com>
2020-07-31 20:42:02 -0300

    feat: adding UserProfileResponseModel

---
+++ src/ReviewApi/ReviewApi.Application/Models/User/UserProfileResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReviewApi.Application.Models.User
+{
+    public class UserProfileResponseModel
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
