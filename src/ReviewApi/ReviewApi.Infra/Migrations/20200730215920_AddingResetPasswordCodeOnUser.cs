42981efb723be21ed787c409f54c59d8bebbae29
Stefan Novasky <stefannovasky@gmail.com>
2020-07-30 21:41:22 -0300

    feat: adding new AddingResetPasswordCodeOnUser migration

---
+++ src/ReviewApi/ReviewApi.Infra/Migrations/20200730215920_AddingResetPasswordCodeOnUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewApi.Infra.Migrations
+{
+    public partial class AddingResetPasswordCodeOnUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "reset_password_code",
+                table: "users",
+                maxLength: 8,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "reset_password_code",
+                table: "users");
+        }
+    }
+}
