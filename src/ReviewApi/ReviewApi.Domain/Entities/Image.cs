9d5929788f9c7eaa0625f24651e285f89c85e1e2
Stefan Novasky <stefannovasky@gmail.com>
2020-08-02 01:41:03 -0300

    feat: adding Image entity

---
+++ src/ReviewApi/ReviewApi.Domain/Entities/Image.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ReviewApi.Domain.Entities
+{
+    public class Image : BaseEntity
+    {
+
+        public string FileName { get; protected set; }
+        public string FilePath { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public User User { get; protected set; }
+
+        public Image() : base()
+        {
+
+        }
+
+        public Image(string fileName, string filePath)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+        }
+
+        public Image(Guid id) : base(id)
+        {
+
+        }
+
+        public Image(Guid id, string fileName, string filePath) : base(id)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+        }
+
+        public Image(string fileName, string filePath, Guid userId)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+            UserId = userId;
+        }
+
+        public Image(Guid id, string fileName, string filePath, Guid userId) : base(id)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+            UserId = userId;
+        }
+    }
+}
