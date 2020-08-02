67fbb3c4daa86eaf9660ff756face5e70e404022
Stefan Novasky <stefannovasky@gmail.com>
2020-08-02 02:05:13 -0300

    feat: adding Image map config

---
+++ src/ReviewApi/ReviewApi.Infra/Mappings/ImageMapConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReviewApi.Domain.Entities;
+
+namespace ReviewApi.Infra.Mappings
+{
+    public class ImageMapConfig : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.ToTable("images");
+
+            builder.Property(p => p.Id).HasColumnName("id");
+
+            builder.Property(p => p.UserId)
+                .HasColumnName("user_id")
+                .HasMaxLength(36)
+                .HasDefaultValue(null);
+
+            builder.Property(p => p.FileName)
+                .HasColumnName("filename")
+                .IsFixedLength()
+                .HasMaxLength(36)
+                .IsRequired();
+
+            builder.Property(p => p.FilePath)
+                .HasColumnName("filepath")
+                .HasMaxLength(300)
+                .IsRequired();
+        }
+    }
+}
