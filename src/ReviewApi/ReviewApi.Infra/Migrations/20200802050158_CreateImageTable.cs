38c971e615af587956829b09367eee7ebdd5f871
Stefan Novasky <stefannovasky@gmail.com>
2020-08-02 02:08:31 -0300

    feat: adding CreateImageTable migration

---
+++ src/ReviewApi/ReviewApi.Infra/Migrations/20200802050158_CreateImageTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewApi.Infra.Migrations
+{
+    public partial class CreateImageTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "image_id",
+                table: "users",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "images",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    filename = table.Column<string>(fixedLength: true, maxLength: 36, nullable: false),
+                    filepath = table.Column<string>(maxLength: 300, nullable: false),
+                    user_id = table.Column<Guid>(maxLength: 36, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_images", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_images_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_images_user_id",
+                table: "images",
+                column: "user_id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "images");
+
+            migrationBuilder.DropColumn(
+                name: "image_id",
+                table: "users");
+        }
+    }
+}
