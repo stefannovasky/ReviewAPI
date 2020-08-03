fe84a8f7ff39a5efd2e048336d704b937fd53d37
Stefan Novasky <stefannovasky@gmail.com>
2020-08-03 17:39:42 -0300

    feat: adding ImageRepository

---
+++ src/ReviewApi/ReviewApi.Domain/Interfaces/Repositories/IImageRepository.cs
@@ -0,0 +1,8 @@
+﻿using ReviewApi.Domain.Entities;
+
+namespace ReviewApi.Domain.Interfaces.Repositories
+{
+    public interface IImageRepository : IGenericRepository<Image>
+    {
+    }
+}
---
+++ src/ReviewApi/ReviewApi.Infra/Repositories/ImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReviewApi.Domain.Entities;
+using ReviewApi.Domain.Interfaces.Repositories;
+using ReviewApi.Infra.Context;
+
+namespace ReviewApi.Infra.Repositories
+{
+    public class ImageRepository : GenericRepository<Image>, IImageRepository
+    {
+        public ImageRepository(MainContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
