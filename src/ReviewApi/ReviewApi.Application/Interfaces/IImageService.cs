ee2ceb679f27dff548435a4020f2942167c77561
Stefan Novasky <stefannovasky@gmail.com>
2020-08-03 21:08:54 -0300

    feat: adding ImageService and GetImageByName method

---
+++ src/ReviewApi/ReviewApi.Application/Interfaces/IImageService.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace ReviewApi.Application.Interfaces
+{
+    public interface IImageService
+    {
+        Stream GetImageByName(string name);
+    }
+}
---
+++ src/ReviewApi/ReviewApi.Application/Services/ImageService.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using ReviewApi.Application.Interfaces;
+using ReviewApi.Domain.Interfaces.Repositories;
+using ReviewApi.Shared.Interfaces;
+
+namespace ReviewApi.Application.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly IFileUploadUtils _fileUploadUtils;
+        public ImageService(IFileUploadUtils fileUploadUtils)
+        {
+            _fileUploadUtils = fileUploadUtils;
+        }
+
+        public Stream GetImageByName(string name)
+        {
+            return _fileUploadUtils.GetImage(name);
+        }
+    }
+}
