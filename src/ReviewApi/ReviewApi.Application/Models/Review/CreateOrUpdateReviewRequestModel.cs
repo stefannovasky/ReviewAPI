61d5686219fa6a18edd01bc85038a3e608476797
Stefan Novasky <stefannovasky@gmail.com>
2020-08-05 19:44:58 -0300

    feat: adding CreateOrUpdateReviewRequestModel

---
+++ src/ReviewApi/ReviewApi.Application/Models/Review/CreateOrUpdateReviewRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace ReviewApi.Application.Models.Review
+{
+    public class CreateOrUpdateReviewRequestModel
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int Stars { get; set; }
+        public Stream Image { get; set; }
+    }
+}
