ea39c04a22c4461d90936d1d221bda8e78781b0a
Stefan Novasky <stefannovasky@gmail.com>
2020-08-05 18:26:32 -0300

    feat: adding Review and ReviewImage entities

---
+++ src/ReviewApi/ReviewApi.Domain/Entities/Review.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReviewApi.Domain.Entities
+{
+    public class Review : BaseEntity
+    {
+        public string Title { get; protected set; }
+        public string Text { get; protected set; }
+        public int Start { get; protected set; }
+        public Guid CreatorId { get; protected set; }
+        public User Creator { get; protected set; }
+        public ReviewImage Image { get; protected set; }
+
+        public Review()
+        {
+
+        }
+
+        public Review(string title, string text, int start, Guid creatorId)
+        {
+            Title = title;
+            Text = text;
+            Start = start;
+            CreatorId = creatorId;
+        }
+
+        public void AddImage(ReviewImage image)
+        {
+            Image = image;
+        }
+    }
+}
---
+++ src/ReviewApi/ReviewApi.Domain/Entities/ReviewImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReviewApi.Domain.Entities
+{
+    public class ReviewImage : BaseImage
+    {
+        public Guid ReviewId { get; protected set; }
+        public Review Review { get; protected set; }
+
+        public ReviewImage()
+        {
+
+        }
+
+        public ReviewImage(string filename, string filepath)
+        {
+            FileName = filename;
+            FilePath = filepath;
+        }
+
+        public void Update(string filename, string filepath)
+        {
+            FileName = filename;
+            FilePath = filepath;
+        }
+    }
+}
