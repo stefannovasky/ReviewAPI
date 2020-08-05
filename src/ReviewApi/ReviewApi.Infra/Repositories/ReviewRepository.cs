563c8545b2bc4790831fa22db07e4563bf8ddb9d
Stefan Novasky <stefannovasky@gmail.com>
2020-08-05 19:11:02 -0300

    feat: adding Review repository

---
+++ src/ReviewApi/ReviewApi.Infra/Repositories/ReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReviewApi.Domain.Entities;
+using ReviewApi.Domain.Interfaces.Repositories;
+using ReviewApi.Infra.Context;
+
+namespace ReviewApi.Infra.Repositories
+{
+    public class ReviewRepository : GenericRepository<Review>, IReviewRepository
+    {
+        public ReviewRepository(MainContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
