bb2a4bf0533a235630efd3a0137817cb44ab81fe
Stefan Novasky <stefannovasky@gmail.com>
2020-08-05 18:32:27 -0300

    test: adding ReviewImage tests

---
+++ src/ReviewApi/ReviewApi.UnitTests/Domain/Entities/ReviewImageTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ReviewApi.Domain.Entities;
+using Xunit;
+
+namespace ReviewApi.UnitTests.Domain.Entities
+{
+    public class ReviewImageTest
+    {
+        [Fact]
+        public void ShouldConstructWithParameterlessConstructor()
+        {
+            ReviewImage reviewImage = new ReviewImage();
+
+            Assert.Null(reviewImage.Review);
+            Assert.Null(reviewImage.FileName);
+            Assert.Null(reviewImage.FilePath);
+            Assert.Equal(Guid.Empty, reviewImage.Id);
+            Assert.Equal(Guid.Empty, reviewImage.ReviewId);
+        }
+
+        [Fact]
+        public void ShouldConstructWithFilenameFilepathConstructor()
+        {
+            string filename = "FILENAME";
+            string filepath = "FILEPATH";
+            ReviewImage reviewImage = new ReviewImage(filename, filepath);
+
+            Assert.Null(reviewImage.Review);
+            Assert.Equal(filename, reviewImage.FileName);
+            Assert.Equal(filepath, reviewImage.FilePath);
+            Assert.Equal(Guid.Empty, reviewImage.Id);
+            Assert.Equal(Guid.Empty, reviewImage.ReviewId);
+        }
+
+        [Fact]
+        public void ShouldUpdate()
+        {
+            string filename = "FILENAME";
+            string filepath = "FILEPATH";
+            ReviewImage reviewImage = new ReviewImage();
+
+            reviewImage.Update(filename, filepath);
+
+            Assert.Null(reviewImage.Review);
+            Assert.Equal(filename, reviewImage.FileName);
+            Assert.Equal(filepath, reviewImage.FilePath);
+            Assert.Equal(Guid.Empty, reviewImage.Id);
+            Assert.Equal(Guid.Empty, reviewImage.ReviewId);
+        }
+    }
+}
