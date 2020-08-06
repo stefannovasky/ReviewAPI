e3601c1047b431b6d115d95c4ae6cd4df8b2aa3a
Stefan Novasky <stefannovasky@gmail.com>
2020-08-05 23:11:08 -0300

    feat: adding CreateOrUpdateReviewModel

---
+++ src/ReviewApi/ReviewApi/Models/CreateOrUpdateReviewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ReviewApi.Models
+{
+    public class CreateOrUpdateReviewModel
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int Stars { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
