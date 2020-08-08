a32d51500f0a553627cb154546897f47b1bec2b9
Stefan Novasky <stefannovasky@gmail.com>
2020-08-07 21:18:51 -0300

    feat: adding ForbiddenException

---
+++ src/ReviewApi/ReviewApi.Domain/Exceptions/ForbiddenException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReviewApi.Domain.Exceptions
+{
+    public class ForbiddenException : Exception
+    {
+        public ForbiddenException() : base()
+        {
+
+        }
+    }
+}
