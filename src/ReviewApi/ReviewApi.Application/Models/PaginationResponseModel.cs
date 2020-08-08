329a1c62f99cb02641a8aae3af6ba366fccc77cf
Stefan Novasky <stefannovasky@gmail.com>
2020-08-07 22:13:11 -0300

    feat: adding PaginationResponseModel

---
+++ src/ReviewApi/ReviewApi.Application/Models/PaginationResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ReviewApi.Application.Models
+{
+    public class PaginationResponseModel<T>
+    {
+        public IEnumerable<T> Data { get; set; }
+        public string NextPage { get; set; }
+        public string PreviousPage { get; set; }
+        public int Total { get; set; }
+    }
+}
