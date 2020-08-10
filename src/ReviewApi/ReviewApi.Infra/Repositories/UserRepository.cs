b785aec47436dc7fdd7f8e1f00f8e97d41721837
Stefan Novasky <stefannovasky@gmail.com>
2020-08-10 15:15:07 -0300

    refac: removing try/catch

--- src/ReviewApi/ReviewApi.Infra/Repositories/UserRepository.cs
+++ src/ReviewApi/ReviewApi.Infra/Repositories/UserRepository.cs
@@ -15,62 +15,27 @@ namespace ReviewApi.Infra.Repositories
 
         public async Task<bool> AlreadyExists(string email)
         {
-            try
-            {
-                return await _dbSet.AnyAsync(user => user.Email == email);     
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await _dbSet.AnyAsync(user => user.Email == email);
         }
 
         public async Task<User> GetByConfirmationCode(string code)
         {
-            try
-            {
-                return await Query().SingleOrDefaultAsync(user => user.ConfirmationCode == code);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await Query().SingleOrDefaultAsync(user => user.ConfirmationCode == code);
         }
 
         public async Task<User> GetByEmail(string email)
         {
-            try
-            {
-                return await Query().SingleOrDefaultAsync(user => user.Email == email);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await Query().SingleOrDefaultAsync(user => user.Email == email);
         }
 
         public async Task<User> GetByIdIncludingImage(Guid id)
         {
-            try
-            {
-                return await Query().Include(user => user.ProfileImage).SingleOrDefaultAsync(user => user.Id == id);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await Query().Include(user => user.ProfileImage).SingleOrDefaultAsync(user => user.Id == id);
         }
 
         public async Task<User> GetByName(string name)
         {
-            try
-            {
-                return await Query().SingleOrDefaultAsync(user => user.Name == name);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await Query().SingleOrDefaultAsync(user => user.Name == name);
         }
 
         public async Task<User> GetByNameIncludingImage(string name)
@@ -80,14 +45,7 @@ namespace ReviewApi.Infra.Repositories
 
         public async Task<User> GetByResetPasswordCode(string code)
         {
-            try
-            {
-                return await Query().SingleOrDefaultAsync(user => user.ResetPasswordCode == code);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await Query().SingleOrDefaultAsync(user => user.ResetPasswordCode == code);
         }
     }
 }
