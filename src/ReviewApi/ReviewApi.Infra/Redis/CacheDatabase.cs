b57a13ca83327f7661b8f71de64ed4a432c54dc2
Stefan Novasky <stefannovasky@gmail.com>
2020-08-11 11:27:13 -0300

    feat: adding CacheDatabase class

---
+++ src/ReviewApi/ReviewApi.Infra/Redis/CacheDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using ReviewApi.Infra.Redis.Interfaces;
+using StackExchange.Redis;
+
+namespace ReviewApi.Infra.Redis
+{
+    public class CacheDatabase : RedisConnector, ICacheDatabase
+    {
+        private readonly IDatabase _cacheDatabase;
+        public CacheDatabase(string connectionString) : base(connectionString)
+        {
+            Connect();
+            _cacheDatabase = Connection.GetDatabase();
+        }
+
+        public async Task Remove(string key)
+        {
+            await _cacheDatabase.KeyDeleteAsync(key);
+        }
+
+        public async Task Set(string key, string jsonValue)
+        {
+            await _cacheDatabase.StringSetAsync(key, jsonValue);
+        }
+
+        public async Task<string> Get(string key)
+        {
+            return await _cacheDatabase.StringGetAsync(key);
+        }
+    }
+}
---
+++ src/ReviewApi/ReviewApi.Infra/Redis/Interfaces/ICacheDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace ReviewApi.Infra.Redis.Interfaces
+{
+    public interface ICacheDatabase : IRedisConnector
+    {
+        Task Set(string key, string jsonValue);
+        Task<string> Get(string key);
+        Task Remove(string key);
+    }
+}
