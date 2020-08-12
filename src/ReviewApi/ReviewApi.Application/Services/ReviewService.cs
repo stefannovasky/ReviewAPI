5209a498cbc9eaa15f606b42c2495f84dc398116
Stefan Novasky <stefannovasky@gmail.com>
2020-08-12 18:17:39 -0300

    refac: removing cache insert on create method

--- src/ReviewApi/ReviewApi.Application/Services/ReviewService.cs
+++ src/ReviewApi/ReviewApi.Application/Services/ReviewService.cs
@@ -43,7 +43,6 @@ namespace ReviewApi.Application.Services
 
             await _reviewRepository.Create(review);
             await _reviewRepository.Save();
-            await _cacheDatabase.Set(review.Id.ToString(), _jsonUtils.Serialize(review));
 
             return new IdResponseModel() { Id = review.Id };
         }
