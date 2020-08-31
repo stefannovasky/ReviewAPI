a0b279b99dd1b2923b44c8401e0901a455527063
Stefan Novasky <stefannovasky@gmail.com>
2020-08-31 19:56:04 -0300

    feat: adding GetByUserIdAndReviewId method

--- src/ReviewApi/ReviewApi.Domain/Interfaces/Repositories/IFavoriteRepository.cs
+++ src/ReviewApi/ReviewApi.Domain/Interfaces/Repositories/IFavoriteRepository.cs
@@ -7,5 +7,6 @@ namespace ReviewApi.Domain.Interfaces.Repositories
     public interface IFavoriteRepository : IGenericRepository<Favorite>
     {
         Task<bool> AlreadyExists(Guid userId, Guid reviewId);
+        Task<Favorite> GetByUserIdAndReviewId(Guid userId, Guid reviewId);
     }
 }
