28f93ba4a1d1f47d16053c975f5df2f3b9e0f2d6
Stefan Novasky <stefannovasky@gmail.com>
2020-08-31 19:11:15 -0300

    feat: adding Create method

---
+++ src/ReviewApi/ReviewApi/Controllers/FavoriteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReviewApi.Application.Interfaces;
+using ReviewApi.Controllers.Extensions;
+
+namespace ReviewApi.Controllers
+{
+    [Route("reviews/{reviewId}/favorites")]
+    [ApiController]
+    public class FavoriteController : ControllerBase
+    {
+        private readonly IFavoriteService _favoriteService; 
+        public FavoriteController(IFavoriteService favoriteService)
+        {
+            _favoriteService = favoriteService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(string reviewId)
+        {
+            try
+            {
+                return Ok(await _favoriteService.Create(this.GetUserIdFromToken(), reviewId));
+            }
+            catch (Exception exception)
+            {
+                return this.HandleExceptionToUserAndLogIfExceptionIsUnexpected(exception);
+            }
+        }
+    }
+}
