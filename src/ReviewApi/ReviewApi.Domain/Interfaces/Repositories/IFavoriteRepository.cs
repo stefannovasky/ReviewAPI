c49a69775b249b8d306e3609e7f5d5dc24494ef6
Stefan Novasky <stefannovasky@gmail.com>
2020-08-31 19:09:11 -0300

    feat: adding FavoriteRepository

---
+++ src/ReviewApi/ReviewApi.Domain/Interfaces/Repositories/IFavoriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using ReviewApi.Domain.Entities;
+
+namespace ReviewApi.Domain.Interfaces.Repositories
+{
+    public interface IFavoriteRepository : IGenericRepository<Favorite>
+    {
+        Task<bool> AlreadyExists(Guid userId, Guid reviewId);
+    }
+}
---
+++ src/ReviewApi/ReviewApi.Infra/Repositories/FavoriteRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReviewApi.Domain.Entities;
+using ReviewApi.Domain.Interfaces.Repositories;
+using ReviewApi.Infra.Context;
+
+namespace ReviewApi.Infra.Repositories
+{
+    public class FavoriteRepository : GenericRepository<Favorite>, IFavoriteRepository
+    {
+        public FavoriteRepository(MainContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<bool> AlreadyExists(Guid userId, Guid reviewId)
+        {
+            return await Query().AnyAsync(favorite => favorite.UserId == userId && favorite.ReviewId == reviewId);
+        }
+    }
+}
