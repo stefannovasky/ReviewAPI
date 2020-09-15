76c55485352c41711fd9f64576c68278d5e03d8d
Stefan Novasky <stefannovasky@gmail.com>
2020-09-15 15:28:49 -0300

    feat: adding Converter

---
+++ src/ReviewApi/ReviewApi.Application/Converter/IConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReviewApi.Application.Models.Review;
+using ReviewApi.Application.Models.User;
+using ReviewApi.Domain.Entities;
+
+namespace ReviewApi.Application.Converter
+{
+    public interface IConverter
+    {
+        ReviewResponseModel ConvertReviewToReviewResponseModel(Review review);
+        ReviewResponseModel ConvertReviewToReviewResponseModel(Review review, string userName);
+        UserProfileResponseModel ConvertUserToUserProfileResponseModel(User user);
+    }
+}
