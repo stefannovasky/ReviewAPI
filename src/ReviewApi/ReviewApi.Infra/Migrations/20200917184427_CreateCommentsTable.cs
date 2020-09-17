648d9bbafccf475ed65ffce68d754fd481e33300
Stefan Novasky <stefannovasky@gmail.com>
2020-09-17 15:50:48 -0300

    feat: adding new migration

---
+++ src/ReviewApi/ReviewApi.Infra/Migrations/20200917184427_CreateCommentsTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReviewApi.Infra.Migrations
+{
+    public partial class CreateCommentsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "comments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    text = table.Column<string>(maxLength: 1500, nullable: false),
+                    review_id = table.Column<Guid>(nullable: false),
+                    user_id = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_comments_reviews_review_id",
+                        column: x => x.review_id,
+                        principalTable: "reviews",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_comments_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_review_id",
+                table: "comments",
+                column: "review_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_user_id",
+                table: "comments",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "comments");
+        }
+    }
+}
