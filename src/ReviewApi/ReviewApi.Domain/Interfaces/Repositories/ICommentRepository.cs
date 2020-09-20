a39d04e3b972f440cb54d695ad462232a732992b
Stefan Novasky <stefannovasky@gmail.com>
2020-09-20 18:41:08 -0300

    feat: adding CommentRepository methods

---
+++ src/ReviewApi/ReviewApi.Domain/Interfaces/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ReviewApi.Domain.Entities;
+
+namespace ReviewApi.Domain.Interfaces.Repositories
+{
+    public interface ICommentRepository : IGenericRepository<Comment>
+    {
+        Task<IEnumerable<Comment>> GetAllFromReview(Guid reviewId, int page, int quantityPerPage);
+        Task<int> CountFromReview(Guid reviewId);
+        Task<Comment> GetByIdIncludingUser(Guid commentId);
+    }
+}
